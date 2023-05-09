50ab636bfe924fedaafb2016d34cc42a0842239c
SwaggyLTW <tommy22@live.unc.edu>
2023-05-09 17:09:47 -0400

    files added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // Create require function 
 // https://nodejs.org/docs/latest-v18.x/api/module.html#modulecreaterequirefilename
 import { createRequire } from 'node:module';
+import {rps, rpsls} from "./lib/rpsls.js";
 const require = createRequire(import.meta.url);
 // The above two lines allow us to use ES methods and CJS methods for loading
 // dependencies.
@@ -62,7 +63,44 @@ const app = express()
 // Set a port for the server to listen on
 const port = args.port || args.p || process.env.PORT || 8080
 // Load app middleware here to serve routes, accept data requests, etc.
-//
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+//default
+app.get('/app', (req, res)=>{
+    res.status(200).send("200 OK");
+});
+//RPS
+app.get('/app/rps', (req, res) =>{
+    res.status(200).send(JSON.stringify(rps()));
+});
+//rpsls
+app.get('/app/rpsls', (req, res) =>{
+    res.status(200).send(JSON.stringify(rpsls()));
+});
+//rps shot
+app.get('/app/rps/play/', (req, res)=>{
+    res.status(200).send(rps(req.query.shot));
+});
+//rpsls shot
+app.get('/app/rpsls/play/', (req, res)=>{
+    res.status(200).send(rpsls(req.query.shot));
+});
+//rps POST request
+app.post('/app/rps/play/', (req, res)=>{
+    res.status(200).send(rps(req.body.shot));
+});
+//rpsls POST request
+app.post('/app/rpsls/play/', (req, res)=>{
+    res.status(200).send(rpsls(req.body.shot));
+});
+//rps URL
+app.get('/app/rps/play/:shot/', (req, res)=>{
+    res.status(200).send(JSON.stringify(rps(req.params.shot)));
+});
+//rpsls URL
+app.get('/app/rpsls/play/:shot/', (req, res)=>{
+    res.status(200).send(JSON.stringify(rpsls(req.params.shot)));
+});
 // Create and update access log
 // The morgan format below is the Apache Foundation combined format but with ISO8601 dates
 app.use(morgan(':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"',
